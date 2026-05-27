d26f7e5d06e166da1df99344f95e855ee8e549dd
miraraeri <milana10004@gmail.com>
2026-05-27 18:08:04 +0300

    Подготовка к запуску

--- iloveuchet/build.sh
+++ iloveuchet/build.sh
@@ -6,6 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 
-python manage.py migrate
+#python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')" | python manage.py shell
